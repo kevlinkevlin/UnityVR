4b293180829f55afa2c8f573aec487cf317074b2
kevlinkevlin <kevin1133557799@gmail.com>
2020-04-13 16:15:40 +0800

    VR game practice

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Damage : MonoBehaviour
+{
+    public float HP = 100.0f;
+    public Image HP_Bar;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Under Attack!");
+        HP -= 25.0f;
+        HP_Bar.fillAmount = HP / 100.0f;
+    }
+}
